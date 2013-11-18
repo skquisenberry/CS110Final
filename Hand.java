a4ed0839edf75ef451b593b173b6ffdb48e0f8b8
skquisenberry <scott.quisenberry@uvm.edu>
2013-11-18 09:49:06 -0500

    Hand class

---
+++ Hand.java
@@ -0,0 +1,28 @@
+
+
+//Scott Quisenberry
+//CS 110
+//Final Project: Hand class
+
+public class Hand extends CardPile
+{
+   
+   /**
+      The constructor creates the CardPile object.
+   */
+   public Hand()
+   {
+      super();
+   }
+   
+   /**
+      The peek method returns a Card without removing it from the ArrayList.
+   */
+   public Card peek(int index)
+   {
+      Card temp = super.get(index);
+      super.add(index, temp);
+      return temp;
+   }
+
+}
