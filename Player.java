574fd0cfdcd6b988d9ac4fc43fc20c22d32759e6
skquisenberry <scott.quisenberry@uvm.edu>
2013-11-18 10:09:48 -0500

    Player class

---
+++ Player.java
@@ -0,0 +1,22 @@
+
+
+//Scott Quisenberry
+//CS 110
+//Final Project: Player class
+
+public class Player
+{
+   
+   public String name;
+   public Hand hand;
+   
+   public Player(String name)
+   {
+      this.name = name;
+      hand = new Hand();
+   }
+   
+   
+   
+
+}
