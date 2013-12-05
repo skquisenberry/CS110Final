31d5b0ad76a28f18ed6cbdcfa9cbdf6dfb297744
skquisenberry <scott.quisenberry@uvm.edu>
2013-12-05 13:17:34 -0500

    ButtonListener
    Added ButtonListener to the WarGUI class

--- WarGUI.java
+++ WarGUI.java
@@ -6,7 +6,7 @@ class WarGUI extends JFrame
 {
    private JPanel top, bottom, titleSpace, cardSpace;
    private JLabel title, cardBack, cardOne, cardTwo;
-   private Game game;
+   private Game2 game;
    
    WarGUI(String s) 
    {
@@ -29,204 +29,29 @@ class WarGUI extends JFrame
       bottom = new JPanel(new GridLayout(1, 1));
       add(bottom);
       JButton button = new JButton("Next Round");
+      button.addActionListener(new ButtonListener());
       bottom.add(button);
 
       
       
    }
    
-   /**
-      The cardInPlay method takes in a Card and sets the panel to the correct card.
-      @param p1 The Card for player 1
-   */
-   public void cardInPlay(Card p1)
+   
+   private class ButtonListener implements ActionListener
    {
-      if(p1.getSuit() == 1)
+      
+      Game2 game;
+      public void actionPerformed(ActionEvent e)
       {
-         switch(p1.getRank())
-         {
-            case 1:
-              
-               break;
-            case 2:
-               
-               break;
-            case 3:
-               
-               break;
-            case 4:
-               
-               break;
-            case 5:
-               
-               break;
-            case 6:
-               
-               break;
-            case 7:
-               
-               break;
-            case 8:
-               
-               break;
-            case 9:
-               
-               break;
-            case 10:
-               
-               break;
-            case 11:
-               
-               break;
-            case 12:
-               
-               break;
-            case 13:
-               
-               break;
-         
-         }
+         game = new Game2();
+         game.turn();
       }
-      else if(p1.getSuit() == 2)
-      {
-         switch(p1.getRank())
-         {
-            case 1:
-              
-               break;
-            case 2:
-               
-               break;
-            case 3:
-               
-               break;
-            case 4:
-               
-               break;
-            case 5:
-               
-               break;
-            case 6:
-               
-               break;
-            case 7:
-               
-               break;
-            case 8:
-               
-               break;
-            case 9:
-               
-               break;
-            case 10:
-               
-               break;
-            case 11:
-               
-               break;
-            case 12:
-               
-               break;
-            case 13:
-               
-               break;
-         }
-      }
-      else if(p1.getSuit() == 3)
-      {
-         switch(p1.getRank())
-         {
-            case 1:
-              
-               break;
-            case 2:
-               
-               break;
-            case 3:
-               
-               break;
-            case 4:
-               
-               break;
-            case 5:
-               
-               break;
-            case 6:
-               
-               break;
-            case 7:
-               
-               break;
-            case 8:
-               
-               break;
-            case 9:
-               
-               break;
-            case 10:
-               
-               break;
-            case 11:
-               
-               break;
-            case 12:
-               
-               break;
-            case 13:
-               
-               break;
-         }
-      }
-      else
-      {
-         switch(p1.getRank())
-         {
-            case 1:
-              
-               break;
-            case 2:
-               
-               break;
-            case 3:
-               
-               break;
-            case 4:
-               
-               break;
-            case 5:
-               
-               break;
-            case 6:
-               
-               break;
-            case 7:
-               
-               break;
-            case 8:
-               
-               break;
-            case 9:
-               
-               break;
-            case 10:
-               
-               break;
-            case 11:
-               
-               break;
-            case 12:
-               
-               break;
-            case 13:
-               
-               break;
-         }
-      }
-           
+   
    }
    
    public static void main(String [] args)
    {
+      Game2 game = new Game2();
       JFrame frame = new WarGUI("Hello");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setSize(500,500);
